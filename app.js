2d4ed573edde71c97e8bd488fd4dc3d35f7421a9
Tim <tjm6014@gmail.com>
2024-05-03 10:43:12 -0700

    initial

---
+++ app.js
@@ -0,0 +1,16 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this!
+
+const PI = 3.14;
+
+/* Quiz
+1.
+var is the least stable sort of variable declaration. You can redeclare variablese defined with var, but not with let. 
+Var variables are also either scoped to the function or added to the window object, while let variables simlpy have block scope. 
+2.
+Var variables can be both redeclared and and reassigned, while const variables can do neither.
+3.
+Let variables can be reassigned, while const variables cannot. Let also allows for variable definition without a value, while const variables must be assigned an itnital value at declaration.
+4.
+Hoisting is the phenomenon in which var variable declarations are "run" first, so their values are undefined for JS until JS reaches the actual variables declaration.
+*/
